f47b939aeaf44710ecf6317e0848c4da4c85efae
dev-xero <ssh.xero@gmail.com>
2026-05-13 14:08:59 +0100

    chore: include webhook router in main

--- api/app/main.py
+++ api/app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
-from app.routers import auth
+from app.routers import auth, webhooks
 
 app = FastAPI(title='Squad Hackathon (2026) API')
 
 app.include_router(auth.router)
+app.include_router(webhooks.router)
 
 
 @app.get('/health', status_code=200)
